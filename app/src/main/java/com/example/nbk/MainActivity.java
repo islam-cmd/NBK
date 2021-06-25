6caa79b8e503d7f531102a718c12cb0c835ab047
islam-cmd <i.tookhy@gmail.com>
2021-06-25 04:45:22 +0300

    Splash screen

--- app/src/main/java/com/example/nbk/MainActivity.java
+++ app/src/main/java/com/example/nbk/MainActivity.java
@@ -2,20 +2,37 @@ package com.example.nbk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 //    FirebaseDatabase database;
+private static int SPLASH_SCREEN_TIME_OUT=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message/test");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+//                Intent i=new Intent(MainActivity.this,
+//                        SecondActivity.class);
+                //Intent is used to switch from one activity to another.
 
-        myRef.setValue("Hello, World!");
+//                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
+
 }
