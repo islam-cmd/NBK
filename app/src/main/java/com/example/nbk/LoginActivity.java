27d6458c673d8b8d4276ccfd6540d0e7ca6257c9
islam-cmd <i.tookhy@gmail.com>
2021-06-25 12:30:47 +0300

    log in and card

--- app/src/main/java/com/example/nbk/LoginActivity.java
+++ app/src/main/java/com/example/nbk/LoginActivity.java
@@ -1,33 +1,77 @@
 package com.example.nbk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
-FrameLayout fl ;
+
+    EditText et_email;
+    EditText et_password;
+    FrameLayout fl_login;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Window w = getWindow();
-        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS );
-        setContentView(R.layout.activity_login);
-fl = findViewById(R.id.btn);
-fl.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        startActivity( new Intent(LoginActivity.this,
-                DashboardActivity.class));
-    }
-});
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        //auto login
+//        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+//        if (firebaseUser != null) {
+//            startActivity(new Intent(LoginActivity.this, SessionListActivity.class));
+//            finish();
+//        }
 
+        setContentView(R.layout.activity_login);
+        et_email = findViewById(R.id.email);
+        et_password = findViewById(R.id.password);
+        fl_login = findViewById(R.id.btn);
+
+        auth = FirebaseAuth.getInstance();
+
+        fl_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email = et_email.getText().toString();
+                String txt_password = et_password.getText().toString();
+                if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                    Toast.makeText(LoginActivity.this, "All Fields are Required", Toast.LENGTH_SHORT).show();
+                } else {
+
+//                    lamtienlong9@gmail.com
+                    auth.signInWithEmailAndPassword(txt_email, txt_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Authentication failed!", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 }
