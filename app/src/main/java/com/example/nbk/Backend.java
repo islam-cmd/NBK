62a09df8a7b792853dd039b4d6db781c193cec5c
Khalid <kfg2000@live.com>
2021-06-25 05:25:03 +0300

    started backend

---
+++ app/src/main/java/com/example/nbk/Backend.java
@@ -0,0 +1,39 @@
+package com.example.nbk;
+
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Backend {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    public void deduct(String UID, float amount){
+        DatabaseReference myRef = database.getReference("Customers/"+UID+"/Balance");
+//        DataSnapshot dataSnapshot = myRef.get().getResult();
+//        float value = dataSnapshot.getValue(float.class);
+//        Log.d("TAG", "Value is: " + value);
+//        myRef.setValue(value-amount);
+//        Log.d("TAG", "Value is: " + (value-amount));
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                float value = dataSnapshot.getValue(float.class);
+                Log.d("TAG", "Value is: " + value);
+                myRef.setValue(value-amount);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("TAG", "Failed to read value.", error.toException());
+            }
+        });
+    }
+}
