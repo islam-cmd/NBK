9e3b82d88ccb71859020138f90c128598bfe33c7
islam-cmd <i.tookhy@gmail.com>
2021-06-25 03:37:02 +0300

    The start of the end game

---
+++ app/src/main/java/com/example/nbk/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.nbk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+//    FirebaseDatabase database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message/test");
+
+        myRef.setValue("Hello, World!");
+    }
+}
